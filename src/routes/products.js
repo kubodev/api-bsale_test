1fcf976b7b3b6c8c3754dc1fab597561a9ca079e
kubodev <moisesmagna@gmail.com>
2020-10-03 13:27:20 -0300

    arreglo API, JOIN con  tabla nombre categoria

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,7 +6,7 @@ const mysqlConnection = require('../database');
 // obtiene todo los productos
 router.get('/', (req, res) => {
     
-  const sql = 'SELECT * FROM product';
+  const sql = 'SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name as "categoriaNom"  FROM product p left join category c ON p.category = c.id';
   
     mysqlConnection.query(sql, (err, rows, fields) => {
       if (err) throw err;
@@ -26,7 +26,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const { id } = req.params; 
 
-    const sql = `SELECT * FROM product WHERE id = '${id}'`;
+    const sql = `SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name as "categoriaNom"  FROM product p left join category c ON p.category = c.id WHERE p.id = '${id}'`;
     
     mysqlConnection.query(sql, (err, rows, fields) => {
       if (err) throw err;
@@ -45,7 +45,7 @@ router.get('/:id', (req, res) => {
 router.get('/categoria/:id', (req, res) => {
   const { id } = req.params; 
 
-  const sql = `SELECT * FROM product WHERE category = '${id}'`;
+  const sql = `SELECT id, name FROM product WHERE category = '${id}'`;
   
   mysqlConnection.query(sql, (err, rows, fields) => {
     if (err) throw err;
@@ -64,7 +64,7 @@ router.get('/producto/:name', (req, res) => {
   
   const { name } = req.params; 
 
-  const sql = `SELECT * FROM product WHERE name like '%${name}%'`;
+  const sql = `SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name as "categoriaNom"  FROM product p left join category c ON p.category = c.id WHERE p.name like '%${name}%'`;
   
   mysqlConnection.query(sql, (err, rows, fields) => {
     if (err) throw err;
@@ -82,7 +82,7 @@ router.get('/producto/:name', (req, res) => {
 // obtiene ofertas
 router.get('/oferta/:name', (req, res) => {
   console.log('entro en oferta')
-  const sql = 'SELECT * FROM product order by  discount DESC limit  4';
+  const sql = 'SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name as "categoriaNom"  FROM product p left join category c ON p.category = c.id order by  p.discount DESC limit  4';
   
     mysqlConnection.query(sql, (err, rows, fields) => {
       if (err) throw err;
